c00505b490a549e5df97bd6b6823200a439940d9
shengchenyang <15538221825@163.com>
2024-03-13 15:57:16 +0800

    chore: update README

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # FontMapster 项目说明
+
+## 简介
+
+`fontmapster` 是一个获取动态字体映射的工具，包含通过非 `ocr` 及 `ocr` 通用的方式来获取动态字体加密的真实映射功能。
+
+## 安装
+
+> 简洁安装，不使用 ocr 的方法时推荐安装此便携版：
+
+```
+pip install fontmapster
+```
+
+> 单使用 rapidocr ocr 的方式时：
+
+```
+pip install fontmapster[rapidocr]
+```
+
+> 单使用 cnocr ocr 的方式时：
+
+```
+pip install fontmapster[cnocr]
+```
+
+> 安装所有依赖的方式：
+
+```
+pip install fontmapster[all]
+```
+
+## 简单示例
+
+> 具体示例请在 tests 的测试文件中查看。
+
+1. 普通场景：
+
+   一般是比较标准的字体文件，可以通过通用的方法高效地得到映射。
+
+2. `ocr` 场景：
+
+   在非标准字体文件，或者在一些字体不常变动的站点想快速得到结果时使用。
+
+> 补充说明：
+
+在 `ocr` 场景中可以很方便地调整字体大小，行间距，列间距等参数来提高 `ocr` 工具识别准确率。
+
+比如以下由两个不同设置得到的图示（可以通过 `show` 参数在调试时使用）。
+
+<div>
+    <img src=".\examples\result1.png" style="float: left; width: 45%">
+    <img src=".\examples\result2.png" style="float: left; width: 45%">
+</div>
--- tests/test_normal.py
+++ tests/test_normal.py
@@ -4,6 +4,10 @@ from tests import tests_dir, to_woff
 
 
 def test_normal_qxb():
+    # 1.qxb search page
+    """
+    qxb.woff2 对应启信宝搜索页面的 e.woff2(可能会变化)。
+    """
     to_woff(
         f"{tests_dir}/fonts/qxb.woff2",
         f"{tests_dir}/fonts/qxb_search.woff",
@@ -15,7 +19,11 @@ def test_normal_qxb():
     word = Tools.match_text(html_text, font_map.cmap_uni_map)
     assert word == "百度在线网络技术（北京）有限公司"
 
-    # qxb detail page
+    # 2.qxb detail page
+    """
+    qxb_detail.woff2 对应启信宝企业详情页面的 nl.woff2(可能会变化);
+    另外，详情页中的 c.woff2 不再举例。
+    """
     to_woff(
         f"{tests_dir}/fonts/qxb_detail.woff2",
         f"{tests_dir}/fonts/qxb_detail.woff",
@@ -28,6 +36,7 @@ def test_normal_qxb():
 
 
 def test_normal_chacewang():
+    # 示例为 jzsc.jst.zj.gov.cn/PublicWeb/index.html 中企业名称的字体
     fm = Mapster(font_path=f"{tests_dir}/fonts/chacewang.woff")
     font_map = fm.get_map()
     html_text = "膣臣舱脑舧舙臹膗腟芹"
